e14d420b1e447d0bb29db5c87a9fca97d21da3e7
Matteo Ciman <wizard88_mc@yahoo.it>
2014-10-11 01:15:46 +0200

    To test file upload

--- WhereIsMySmartphone/src/main/java/it/cs/unipd/whereismysmartphone/MainActivity.java
+++ WhereIsMySmartphone/src/main/java/it/cs/unipd/whereismysmartphone/MainActivity.java
@@ -1,5 +1,6 @@
 package it.cs.unipd.whereismysmartphone;
 
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -7,7 +8,6 @@ import android.media.Ringtone;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.Parcelable;
 import android.os.Vibrator;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
@@ -18,12 +18,16 @@ import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -35,10 +39,10 @@ import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends ActionBarActivity
 {
-
-    public static boolean recording = false;
-
     private Controller mController = null;
+    private ProgressDialog dialog = null;
+    private static final String UPLOAD_SERVER_URI = "";
+    private int serverResponseCode = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -106,72 +110,67 @@ public class MainActivity extends ActionBarActivity
             clearDb();
             return true;
         }
-        else if (id == R.id.action_upload_db) {
+        else if (id == R.id.action_send_db) {
             shareDb();
+            return true;
+        }
+        else if (id == R.id.action_upload_db) {
+            uploadFiles();
             return true;
         }
         return (super.onOptionsItemSelected(item));
     }
 
-    public void onBtnClick(final View view) {
+    public void onBtnClick(final View view)
+    {
+        Log.d("MAIN_ACTIVITY", "onBtnClick");
 
-        if (!recording) {
+        final String sex = ((Spinner) findViewById(R.id.sex)).getSelectedItem().toString();
+        final String age = ((Spinner) findViewById(R.id.age)).getSelectedItem().toString();
+        final String height = ((Spinner) findViewById(R.id.height)).getSelectedItem().toString();
+        final String shoes = ((Spinner) findViewById(R.id.shoes)).getSelectedItem().toString();
+        final String hand = ((Spinner) findViewById(R.id.hand)).getSelectedItem().toString();
+        final String action = ((Spinner) findViewById(R.id.action)).getSelectedItem().toString();
+        final String origin = ((Spinner) findViewById(R.id.origin)).getSelectedItem().toString();
+        final String destination = ((Spinner) findViewById(R.id.destination)).getSelectedItem().toString();
 
-            Log.d("MAIN_ACTIVITY", "onBtnClick");
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                view.findViewById(R.id.btnStartStop).setEnabled(false);
+                new Thread() {
+                    public void run() {
+                        mController.readyToStartExercise(sex, age, height, shoes, hand, action,
+                                origin, destination);
 
-            final String sex = ((Spinner) findViewById(R.id.sex)).getSelectedItem().toString();
-            final String age = ((Spinner) findViewById(R.id.age)).getSelectedItem().toString();
-            final String height = ((Spinner) findViewById(R.id.height)).getSelectedItem().toString();
-            final String shoes = ((Spinner) findViewById(R.id.shoes)).getSelectedItem().toString();
-            final String hand = ((Spinner) findViewById(R.id.hand)).getSelectedItem().toString();
-            final String action = ((Spinner) findViewById(R.id.action)).getSelectedItem().toString();
-            final String origin = ((Spinner) findViewById(R.id.origin)).getSelectedItem().toString();
-            final String destination = ((Spinner) findViewById(R.id.destination)).getSelectedItem().toString();
+                        ScheduledExecutorService executor = Executors.newScheduledThreadPool(1);
+                        executor.schedule(new Runnable() {
+                            @Override
+                            public void run() {
+                                playSoundAndVibrate();
+                            }
+                        }, 4, TimeUnit.SECONDS);
 
-            recording = true;
+                        ScheduledExecutorService secondExecutor = Executors.newScheduledThreadPool(1);
+                        secondExecutor.schedule(new Runnable() {
+                            @Override
+                            public void run() {
 
-            runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-                    view.findViewById(R.id.btnStartStop).setEnabled(false);
-                    new Thread() {
-                        public void run() {
-                            mController.readyToStartExercise(sex, age, height, shoes, hand, action,
-                                    origin, destination);
+                                runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        mController.stopRecording();
+                                        MainActivity.this.findViewById(R.id.btnStartStop).setEnabled(true);
+                                        playSoundAndVibrate();
+                                    }
+                                });
 
-                            ScheduledExecutorService executor = Executors.newScheduledThreadPool(1);
-                            executor.schedule(new Runnable() {
-                                @Override
-                                public void run() {
-                                    playSoundAndVibrate();
-                                }
-                            }, 4, TimeUnit.SECONDS);
-
-                            ScheduledExecutorService secondExecutor = Executors.newScheduledThreadPool(1);
-                            secondExecutor.schedule(new Runnable() {
-                                @Override
-                                public void run() {
-
-                                    runOnUiThread(new Runnable() {
-                                        @Override
-                                        public void run() {
-                                            mController.stopRecording();
-                                            MainActivity.this.findViewById(R.id.btnStartStop).setEnabled(true);
-                                            playSoundAndVibrate();
-                                        }
-                                    });
-
-                                }
-                            }, 8, TimeUnit.SECONDS);
-                        }
-                    }.start();
-                }
-            });
-        }
-        else {
-            recording = false;
-
-        }
+                            }
+                        }, 8, TimeUnit.SECONDS);
+                    }
+                }.start();
+            }
+        });
     }
 
     private void copyFile(InputStream in, OutputStream out) throws IOException {
@@ -238,4 +237,144 @@ public class MainActivity extends ActionBarActivity
         Vibrator v = (Vibrator)getApplicationContext().getSystemService(Context.VIBRATOR_SERVICE);
         v.vibrate(500);
     }
+
+    public int uploadFiles()
+    {
+        HttpURLConnection conn;
+        DataOutputStream dos;
+        String lineEnd = "\r\n";
+        String twoHyphens = "--";
+        String boundary = "*****";
+        int bytesRead, bytesAvailable, bufferSize;
+        byte[] buffer;
+        int maxBufferSize = 1 * 1024 * 1024;
+
+        final String[] files = {Logger.BASE_FILE_NAME_ACCELEROMETER, Logger.BASE_FILE_NAME_LINEAR,
+                Logger.BASE_FILE_NAME_SETTINGS_TRUNK};
+
+        File sourceFile = new File(getExternalFilesDir(null), files[0]);
+
+        if (!sourceFile.isFile()) {
+
+            dialog.dismiss();
+
+            runOnUiThread(new Runnable() {
+                public void run() {
+                    Toast.makeText(MainActivity.this, "Source File does not exists: " + files[0], Toast.LENGTH_LONG);
+                }
+            });
+
+            return 0;
+
+        }
+        else
+        {
+            try {
+
+                // open a URL connection to the Servlet
+                FileInputStream fileInputStream = new FileInputStream(sourceFile);
+                URL url = new URL(UPLOAD_SERVER_URI);
+
+                // Open a HTTP  connection to  the URL
+                conn = (HttpURLConnection) url.openConnection();
+                conn.setDoInput(true); // Allow Inputs
+                conn.setDoOutput(true); // Allow Outputs
+                conn.setUseCaches(false); // Don't use a Cached Copy
+                conn.setRequestMethod("POST");
+                conn.setRequestProperty("Connection", "Keep-Alive");
+                conn.setRequestProperty("ENCTYPE", "multipart/form-data");
+                conn.setRequestProperty("Content-Type", "multipart/form-data;boundary=" + boundary);
+                conn.setRequestProperty("uploaded_file", files[0]);
+
+                dos = new DataOutputStream(conn.getOutputStream());
+
+                dos.writeBytes(twoHyphens + boundary + lineEnd);
+                dos.writeBytes("Content-Disposition: form-data; name=\"uploaded_file\";filename=\""
+                                + files[0] + "\"" + lineEnd);
+                dos.writeBytes(lineEnd);
+
+                // create a buffer of  maximum size
+                bytesAvailable = fileInputStream.available();
+
+                bufferSize = Math.min(bytesAvailable, maxBufferSize);
+                buffer = new byte[bufferSize];
+
+                // read file and write it into form...
+                bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+
+                while (bytesRead > 0) {
+
+                    dos.write(buffer, 0, bufferSize);
+                    bytesAvailable = fileInputStream.available();
+                    bufferSize = Math.min(bytesAvailable, maxBufferSize);
+                    bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+
+                }
+
+                // send multipart form data necesssary after file data...
+                dos.writeBytes(lineEnd);
+                dos.writeBytes(twoHyphens + boundary + twoHyphens + lineEnd);
+
+                // Responses from the server (code and message)
+                serverResponseCode = conn.getResponseCode();
+                String serverResponseMessage = conn.getResponseMessage();
+
+                Log.i("uploadFile", "HTTP Response is : "
+                        + serverResponseMessage + ": " + serverResponseCode);
+
+                if(serverResponseCode == 200){
+
+                    runOnUiThread(new Runnable() {
+                        public void run() {
+
+                            String msg = "File Upload Completed.\n\n See uploaded file here : \n\n"
+                                    +" http://www.androidexample.com/media/uploads/"
+                                    +files[0];
+
+                            Toast.makeText(MainActivity.this, "File Upload Complete.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
+
+                //close the streams //
+                fileInputStream.close();
+                dos.flush();
+                dos.close();
+
+            } catch (MalformedURLException ex) {
+
+                dialog.dismiss();
+                ex.printStackTrace();
+
+                runOnUiThread(new Runnable() {
+                    public void run() {
+                        Log.d("EXCEPTION_UPLOAD", "MalformedURLException Exception : check script url.");
+                        Toast.makeText(MainActivity.this, "MalformedURLException",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+                Log.e("Upload file to server", "error: " + ex.getMessage(), ex);
+            } catch (Exception e) {
+
+                dialog.dismiss();
+                e.printStackTrace();
+
+                runOnUiThread(new Runnable() {
+                    public void run()
+                    {
+                        Toast.makeText(MainActivity.this, "Got Exception : see logcat ",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                });
+                Log.e("Upload file to server Exception", "Exception : "
+                        + e.getMessage(), e);
+            }
+            dialog.dismiss();
+            return serverResponseCode;
+
+        } // End else block
+    }
 }
+
